e32a41568197aa82e9b08e091d767c826a12506a
Nurnasib <binsharf15@gmail.com>
2022-02-22 13:08:49 +0600

    jejej

---
+++ third-project/app/Http/Controllers/StudentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    protected $students;
+
+
+    public function index()
+    {
+        $this->student = new Student();
+        $this->student->newStudent();
+        return 'success';
+
+//        return view('all', ['students' => $this->students]);
+    }
+}
